f93b4eb364f1ca7a79026c40bc347106bfb14aaa
Guo Du <mrduguo@gmail.com>
2016-09-04 22:34:45 +0100

    added splash bg

--- demo/tools/dist-utils.js
+++ demo/tools/dist-utils.js
@@ -3,12 +3,16 @@ var ncp = require('ncp');
 var pug = require('pug');
 var path = require('path');
 var rimraf = require('rimraf');
+var replace = require('replace');
 
 var publicPath = path.join(process.cwd(), 'public');
 var distPath = path.join(process.cwd(), 'dist');
 var indexPath = path.join(process.cwd(), 'views', 'index.pug');
 var destIndexPath = path.join(distPath, 'index.html');
 var destIndexRTLPath = path.join(distPath, 'index-rtl.html');
+var jsPath = path.join(distPath, 'js');
+var cssPath = path.join(distPath, 'css');
+var jsAppPath = path.join(jsPath, 'app.js');
 
 module.exports = function(callback) {
   rimraf(distPath, function() {
@@ -24,17 +28,49 @@ module.exports = function(callback) {
       });
 
       var html = fn({
-        app_scripts: "\n    <script src='/js/plugins.js'></script>\n    <script src='/js/app.js'></script>",
-        app_stylesheets: "\n    <link rel='stylesheet' href='/css/main.css' />"
+        app_scripts: "\n    <script src='js/plugins.js'></script>\n    <script src='js/app.js'></script>",
+        app_stylesheets: "\n    <link rel='stylesheet' href='css/main.css' />"
       });
 
       var htmlRTL = fn({
-        app_scripts: "\n    <script src='/js/plugins.js'></script>\n    <script src='/js/app.js'></script>",
-        app_stylesheets: "\n    <link rel='stylesheet' href='/css/main-rtl.css' />"
+        app_scripts: "\n    <script src='js/plugins.js'></script>\n    <script src='js/app.js'></script>",
+        app_stylesheets: "\n    <link rel='stylesheet' href='css/main-rtl.css' />"
       });
 
       fs.writeFileSync(destIndexPath, html + '\n');
       fs.writeFileSync(destIndexRTLPath, htmlRTL + '\n');
+
+
+
+      console.log('Replacing!');
+      replace({
+        regex: '"/locales/"',
+        replacement: '"locales/"',
+        paths: [jsPath],
+        recursive: true,
+        silent: false,
+      });
+      replace({
+        regex: '/\./fonts',
+        replacement: '../fonts',
+        paths: [cssPath],
+        recursive: true,
+        silent: false,
+      });
+      replace({
+        regex: '"/imgs/',
+        replacement: '"../imgs/',
+        paths: [cssPath],
+        recursive: true,
+        silent: false,
+      });
+      replace({
+        regex: '/imgs/',
+        replacement: 'imgs/',
+        paths: [jsPath],
+        recursive: true,
+        silent: false,
+      });
 
       console.log('Done!');
 
